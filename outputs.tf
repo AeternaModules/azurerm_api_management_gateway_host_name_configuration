3c48003c6c7ad18bfb7b6e814f54385d785f5ace
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:46:40 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_gateway_host_name_configurations" {
+  description = "All api_management_gateway_host_name_configuration resources"
+  value       = azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations
+}
+
