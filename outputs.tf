60e5591035cd9b45ad2272dd0dd8c8e477f4371d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:45:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "api_management_gateway_host_name_configurations_id" {
   description = "Map of id values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.id }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_gateway_host_name_configurations_api_management_id" {
   description = "Map of api_management_id values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.api_management_id }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.api_management_id if v.api_management_id != null && length(v.api_management_id) > 0 }
 }
 output "api_management_gateway_host_name_configurations_certificate_id" {
   description = "Map of certificate_id values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.certificate_id }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.certificate_id if v.certificate_id != null && length(v.certificate_id) > 0 }
 }
 output "api_management_gateway_host_name_configurations_gateway_name" {
   description = "Map of gateway_name values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.gateway_name }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.gateway_name if v.gateway_name != null && length(v.gateway_name) > 0 }
 }
 output "api_management_gateway_host_name_configurations_host_name" {
   description = "Map of host_name values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.host_name }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.host_name if v.host_name != null && length(v.host_name) > 0 }
 }
 output "api_management_gateway_host_name_configurations_http2_enabled" {
   description = "Map of http2_enabled values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.http2_enabled }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.http2_enabled if v.http2_enabled != null }
 }
 output "api_management_gateway_host_name_configurations_name" {
   description = "Map of name values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.name }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "api_management_gateway_host_name_configurations_request_client_certificate_enabled" {
   description = "Map of request_client_certificate_enabled values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.request_client_certificate_enabled }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.request_client_certificate_enabled if v.request_client_certificate_enabled != null }
 }
 output "api_management_gateway_host_name_configurations_tls10_enabled" {
   description = "Map of tls10_enabled values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.tls10_enabled }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.tls10_enabled if v.tls10_enabled != null }
 }
 output "api_management_gateway_host_name_configurations_tls11_enabled" {
   description = "Map of tls11_enabled values across all api_management_gateway_host_name_configurations, keyed the same as var.api_management_gateway_host_name_configurations"
-  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.tls11_enabled }
+  value       = { for k, v in azurerm_api_management_gateway_host_name_configuration.api_management_gateway_host_name_configurations : k => v.tls11_enabled if v.tls11_enabled != null }
 }
 
